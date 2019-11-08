9e6937830e45fd5e54d32fb52b1e3d25e98db1b0
psidders <57272321+psidders@users.noreply.github.com>
2019-11-07 21:54:37 -0600

    Added Drop form for when a weapon is dropped

---
+++ Implementation/GenericRPG/FrmDrop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameLibrary;
+using System.Media;
+
+namespace GenericRPG
+{
+    public partial class FrmDrop : Form
+    {
+        public SoundPlayer sp;
+        public FrmDrop()
+        {
+            InitializeComponent();
+            sp = new SoundPlayer(Properties.Resources.angels);
+            sp.Play();
+            
+        }
+        private void FrmDrop_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+    }
+}
